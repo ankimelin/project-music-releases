944630aef0ae7eb4d37c331008e4e7a529981c2a
Annika Melin <melin.anki>
2020-10-16 01:10:11 +0200

    added conditionals to decide what to display for artists

--- code/src/Album.js
+++ code/src/Album.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Artist } from './Artist.js'
 
-
 export const Album = (props) => {
   return (
     <article className="album">
@@ -10,8 +9,8 @@ export const Album = (props) => {
       </a>
       <h1>{props.name}</h1>
       <div>
-        {props.artists.map(artist => {
-          return <Artist key={artist.id} name={artist.name} link={artist.external_urls.spotify} />
+        {props.artists.map((artist, index, artists) => {
+          return <Artist key={artist.id} name={artist.name} link={artist.external_urls.spotify} index={index} artists={artists} />
         })}
       </div>
     </article>
--- code/src/Artist.js
+++ code/src/Artist.js
@@ -1,11 +1,64 @@
 import React from 'react'
 
 export const Artist = props => {
-  return (
-    <div>
-      <a href={props.link} target="_blank">
-        <h2>{props.name}</h2>
-      </a>
-    </div>
-  )
+  if (props.index === 0 && props.artists.length === 1) {
+    return (
+      <div className="artist">
+        <a href={props.link} target="_blank">
+          <h2>{props.name}</h2>
+        </a>
+      </div>
+    )
+  }
+  if (props.index === 0 && props.artists.length === 2) {
+    return (
+      <div className="artist">
+        <a href={props.link} target="_blank">
+          <h2>{props.name} &</h2>
+        </a>
+      </div>
+    )
+  }
+  if (props.index === 1 && props.artists.length === 2) {
+    return (
+      <div className="artist">
+        <a href={props.link} target="_blank">
+          <h2>{props.name}</h2>
+        </a>
+      </div>
+    )
+  }
+  if (props.index === 0 && props.artists.length > 2) {
+    return (
+      <div className="artist">
+        <a href={props.link} target="_blank">
+          <h2>{props.name},</h2>
+        </a>
+      </div>
+    )
+  }
+  if (props.index === 1 && props.artists.length > 2) {
+    return (
+      <div className="artist">
+        <a href={props.link} target="_blank">
+          <h2>{props.name} & </h2>
+        </a>
+      </div>
+    )
+  }
+  if (props.index === 2 && props.artists.length > 2) {
+    return (
+      <div className="artist">
+        <a href={props.link} target="_blank">
+          <h2>{props.name}</h2>
+        </a>
+      </div>
+    )
+  }
+  else {
+    return (
+      null
+    )
+  }
 }
+
