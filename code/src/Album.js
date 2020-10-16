58c66bbad2ffcd4f7c2ad233a0bd5bb009054b4d
Annika Melin <melin.anki>
2020-10-16 03:17:50 +0200

    added icon and hover effects

--- code/src/Album.js
+++ code/src/Album.js
@@ -5,14 +5,19 @@ export const Album = (props) => {
   return (
     <article className="album">
       <a href={props.link} target="_blank">
-        <img src={props.image} alt="cover"></img>
+        <div class="parent">
+          <img class="cover" src={props.image} alt="cover"></img>
+          <img class="play" src='https://cdn0.iconfinder.com/data/icons/google-material-design-3-0/48/ic_play_circle_filled_white_48px-512.png'></img>
+        </div>
       </a>
-      <h1>{props.name}</h1>
+      <a href={props.link} target="_blank">
+        <h1>{props.name}</h1>
+      </a>
       <div>
         {props.artists.map((artist, index, artists) => {
           return <Artist key={artist.id} name={artist.name} link={artist.external_urls.spotify} index={index} artists={artists} />
         })}
       </div>
-    </article>
+    </article >
   )
 }
--- code/src/App.js
+++ code/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import data from './data.json'
 import { Album } from './Album.js'
+import { Header } from './Header.js'
 
 export const App = () => {
   return (
-    <div className="album-container">
-      {data.albums.items.map(album => {
-        return <Album key={album.id} name={album.name} image={album.images[0].url} link={album.external_urls.spotify} artists={album.artists} />
-      })}
+    <div> <Header />
+      <div className="album-container">
+        {data.albums.items.map(album => {
+          return <Album key={album.id} name={album.name} image={album.images[0].url} link={album.external_urls.spotify} artists={album.artists} />
+        })}
+      </div>
     </div>
   )
 }
