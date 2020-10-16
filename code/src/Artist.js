c296d25c9451ddcaec43131cadcc47be54395f85
Annika Melin <melin.anki>
2020-10-16 20:12:54 +0200

    updated artist component conditional

--- code/src/Artist.js
+++ code/src/Artist.js
@@ -5,11 +5,10 @@ import './artist.css'
 const after = (index, artists) => {
   if (index === artists.length - 1) {
     return null
-  }
-  if (index === artists.length - 2) {
-    return <p class="and">&</p>
+  } if (index === artists.length - 2) {
+    return <p>&nbsp;&&nbsp;</p>
   } else {
-    return <p class="comma">,</p>
+    return <p>,&nbsp;</p>
   }
 }
 
@@ -22,67 +21,5 @@ export const Artist = ({ name, link, index, artists }) => {
       {after(index, artists)}
     </div>
   )
-
-  // if (artists.length === 1 && index === 0) {
-  //   return (
-  //     <div className="artist">
-  //       <a href={link} target="_blank" rel="noopener noreferrer" >
-  //         <h2>{name}</h2>
-  //       </a>
-  //     </div>
-  //   )
-  // }
-  // if (artists.length === 2 && index === 0) {
-  //   return (
-  //     <div className="artist">
-  //       <a href={link} target="_blank" rel="noopener noreferrer" >
-  //         <h2>{name}</h2>
-  //       </a>
-  //       <p>&</p>
-  //     </div>
-  //   )
-  // }
-  // if (artists.length === 2 && index === 1) {
-  //   return (
-  //     <div className="artist">
-  //       <a href={link} target="_blank" rel="noopener noreferrer" >
-  //         <h2>{name}</h2>
-  //       </a>
-  //     </div>
-  //   )
-  // }
-  // if (artists.length === 3 && index === 0) {
-  //   return (
-  //     <div className="artist">
-  //       <a href={link} target="_blank" rel="noopener noreferrer" >
-  //         <h2>{name},</h2>
-  //       </a>
-  //     </div>
-  //   )
-  // }
-  // if (artists.length === 3 && index === 1) {
-  //   return (
-  //     <div className="artist">
-  //       <a href={link} target="_blank" rel="noopener noreferrer" >
-  //         <h2>{name}</h2>
-  //       </a>
-  //       <p>&</p>
-  //     </div>
-  //   )
-  // }
-  // if (artists.length === 3 && index === 2) {
-  //   return (
-  //     <div className="artist">
-  //       <a href={link} target="_blank" rel="noopener noreferrer" >
-  //         <h2>{name}</h2>
-  //       </a>
-  //     </div>
-  //   )
-  // }
-  // else {
-  //   return (
-  //     null
-  //   )
-  // }
 }
 
